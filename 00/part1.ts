fbc2c1df496b84de0f4cd2c336601684c17eadf3
simardo <simardo@gmail.com>
2022-11-30 20:11:35 -0500

    chore: initial setup

---
+++ 00/part1.ts
@@ -0,0 +1,37 @@
+// import { INPUT } from './input.ts';
+
+const TEST1 = ``;
+
+function doPart(input: string): string | number {
+    const lines: string[] = input.split('\n').map(s => s);
+    const nums: number[] = input.split('\n').map(s => Number.parseInt(s));
+    const map: number[][] = input.split('\n').map(s => [...s].map(n => Number.parseInt(n)));
+
+    console.log(map.map(m => m.join('')).join('\n'));
+
+    return 0;
+}
+
+function go(input: string, expected?: number | string): void {
+    const result: number | string = doPart(input);
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST1, 0);
+    // go(INPUT);
+})();
+
+/*
+const RE: RegExp = /(\d+)\s<->\s(.+)/g;
+
+let match: RegExpMatchArray | null;
+while ((match = RE.exec(input)) !== null) {
+    const [, id, ids] = match;
+}
+*/
+
+// const render: () => void = () => {
+//     console.log(map.map(line => line.map(cell => cell.actual === 1 ? '>' : cell.actual === 2 ? 'v' : '.').join('')).join('\n'));
+// };
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # adventofcode2022
+
+```deno run ./<day>/partx.ts```
+
+## Example
+
+```deno run ./01/part1.ts```
+
+## Debug
+
+```deno run --inspect-brk ./<day>/partx.ts```
+
+Press `F5` in VSCode
