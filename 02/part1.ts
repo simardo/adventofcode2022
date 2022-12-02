93f20e930042787d20a78122ca99173c8272a0f3
simardo <simardo@gmail.com>
2022-12-02 06:36:08 -0500

    day 02 - :facepunch: :raised_hand: :v:

---
+++ 02/part1.ts
@@ -0,0 +1,46 @@
+import { INPUT } from './input.ts';
+
+const TEST1 = `A Y
+B X
+C Z`;
+
+type Dict<T> = { [key: string]: T };
+
+const eq: Dict<string> = {
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z'
+};
+
+const win: Dict<string> = {
+    'A': 'Y',
+    'B': 'Z',
+    'C': 'X'
+};
+
+const score: Dict<number> = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+};
+
+function doPart(input: string): string | number {
+    const result: number = input.split('\n').reduce((p, v) => {
+        const [opp, me] = v.split(' ');
+
+        return p + score[me] + (eq[opp] === me ? 3 : win[opp] === me ? 6 : 0);
+    }, 0);
+
+    return result;
+}
+
+function go(input: string, expected?: number | string): void {
+    const result: number | string = doPart(input);
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST1, 15);
+    go(INPUT);
+})();
---
+++ 02/part2.ts
@@ -0,0 +1,60 @@
+import { INPUT } from './input.ts';
+
+const TEST1 = `A Y
+B X
+C Z`;
+
+type Dict<T> = { [key: string]: T };
+
+const win: Dict<string> = {
+    'A': 'Y',
+    'B': 'Z',
+    'C': 'X'
+};
+
+const draw: Dict<string> = {
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z'
+};
+
+const lose: Dict<string> = {
+    'A': 'Z',
+    'B': 'X',
+    'C': 'Y'
+};
+
+const score: Dict<number> = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+};
+
+function doPart(input: string): string | number {
+    const result: number = input.split('\n').reduce((p, v) => {
+        const [opp, exp] = v.split(' ');
+
+        if (exp === 'Z') {
+            p += score[win[opp]] + 6;
+        } else if (exp === 'Y') {
+            p += score[draw[opp]] + 3;
+        } else {
+            p += score[lose[opp]];
+        }
+
+        return p;
+    }, 0);
+
+    return result;
+}
+
+function go(input: string, expected?: number | string): void {
+    const result: number | string = doPart(input);
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST1, 12);
+    go(INPUT);
+})();
