5d698ff4bfbabaf17c1e3698e5f1a85a40773d86
simardo <simardo@gmail.com>
2022-12-05 06:47:16 -0500

    day 05 - 🏗️ 📦 pile 📦 dépile 📦

---
+++ 05/part1.ts
@@ -0,0 +1,67 @@
+import { INPUT } from './input.ts';
+
+const TEST1 = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
+function doPart(input: string): string | number {
+    const lines: string[] = input.split('\n').map(s => s);
+
+    const stacks: string[][] = [];
+
+    const RE_STACK = /\[(\w)\]/g;
+
+    lines.forEach(l => {
+        let match: RegExpMatchArray | null;
+        while ((match = RE_STACK.exec(l)) !== null) {
+            const [_, id] = match;
+            const stackIndex = match.index! / 4;
+            
+            while (stacks.length <= stackIndex) {
+                stacks.push([]);
+            }
+
+            const stack: string[] = stacks[stackIndex];
+            stack.splice(0, 0, id);
+        }
+    })
+
+    const RE_INSTR = /move (\d+) from (\d+) to (\d+)/g;
+
+    lines.forEach(l => {
+        let match: RegExpMatchArray | null;
+        while ((match = RE_INSTR.exec(l)) !== null) {
+            const [_, qtyS, fromS, toS] = match;
+            const qty = Number.parseInt(qtyS);
+            const from = Number.parseInt(fromS) - 1;
+            const to = Number.parseInt(toS) - 1;
+            
+            const stackFrom: string[] = stacks[from];
+            const stackTo: string[] = stacks[to];
+
+            for (let q = 1; q <= qty; q++) {
+                stackTo.push(stackFrom.pop()!);
+            }
+            
+        }
+    })
+
+    return stacks.map(s => s.pop()).join('');
+}
+
+function go(input: string, expected?: number | string): void {
+    const result: number | string = doPart(input);
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST1, 'CMZ');
+    go(INPUT);
+})();
