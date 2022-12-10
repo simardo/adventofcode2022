bc3f95e91e1deca3d142bda02a04866a339bfce8
simardo <simardo@gmail.com>
2022-12-10 17:43:20 -0500

    day 10 - :tv: sprites.render()

---
+++ 10/part2.ts
@@ -0,0 +1,195 @@
+import { INPUT } from './input.ts';
+
+const TEST1 = `noop
+addx 3
+addx -5`;
+
+const TEST2 = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`;
+
+function doPart(input: string): string | number {
+    const lines: string[] = input.split('\n');
+
+    let cycle = 0;
+    let crt = '';
+    let sprite = 1;
+
+    lines.forEach(l => {
+        const [instr, valueS] = l.split(' ');
+        const value = Number.parseInt(valueS);
+
+        for (let i = 1; i <= (instr === 'noop' ? 1 : 2); i++) {
+            cycle++;
+            const pos: number = (cycle % 40) - 1;
+            crt += (pos >= sprite - 1 && pos <= sprite + 1) ? "#" : " ";
+
+            if (cycle % 40 === 0) {
+                console.log(crt);
+                crt = '';
+            }
+        }
+        
+        if (value) {
+            sprite += value;
+        }
+    });
+
+    return 0;
+}
+
+function go(input: string, expected?: number | string): void {
+    // console.time();
+    const result: number | string = doPart(input);
+    // console.timeEnd();
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST2, 0);
+    go(INPUT);
+})();
