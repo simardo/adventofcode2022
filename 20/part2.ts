03d9e262e5c03ca28d5775957630efe71ba08878
simardo <simardo@gmail.com>
2022-12-20 21:45:22 -0500

    day 20 - 🔗 ZW5jb2RlZA==

---
+++ 20/part2.ts
@@ -0,0 +1,84 @@
+import { INPUT } from './input.ts';
+
+const TEST1 = `1
+2
+-3
+3
+-2
+0
+4`;
+
+type Link = {
+    value: number;
+    prev: Link;
+    next: Link;
+}
+
+function doPart(input: string): string | number {
+    const nums: Link[] = input.split('\n').map(s => {
+        return {
+            value: Number.parseInt(s) * 811589153
+        } as Link;
+    });
+
+    nums.forEach((l, i) => {
+        l.prev = i === 0 ? nums[nums.length - 1] : nums[i - 1];
+        l.next = i === nums.length - 1 ? nums[0] : nums[i + 1];
+    });
+
+    const chain: Link = nums[0];
+
+    for (let iter = 1; iter <= 10; iter++) {
+        nums.forEach(n => {
+            if (n.value != 0) {
+                // remove
+                n.prev.next = n.next;
+                n.next.prev = n.prev;
+
+                let pos: Link = n;
+                const move: number = n.value % (nums.length - 1);
+                if (pos.value > 0) {
+                    for (let i = 1; i <= move; i++) {
+                        pos = pos.next;
+                    }
+                } else if (pos.value < 0) {
+                    for (let i = 0; i >= move; i--) {
+                        pos = pos.prev;
+                    }
+                }
+
+                const oldPosNext: Link = pos.next;
+                n.next = pos.next;
+                oldPosNext.prev = n;
+
+                n.prev = pos;
+                pos.next = n;
+            }
+        });
+    }
+
+    let zero: Link = nums.find(n => n.value === 0)!;
+
+    let result = 0;
+    for (let i = 1; i <= 3000; i++) {
+        zero = zero.next;
+        if (i % 1000 === 0) {
+            result += zero.value
+        }
+    }
+
+    return result;
+}
+
+function go(input: string, expected?: number | string): void {
+    // console.time();
+    const result: number | string = doPart(input);
+    // console.timeEnd();
+    console.log(`--> ${result} <--`);
+    console.assert(!expected || result === expected, 'got: %s, expected: %s', result, expected);
+}
+
+void (() => {
+    go(TEST1, 1623178306);
+    go(INPUT);
+})();
